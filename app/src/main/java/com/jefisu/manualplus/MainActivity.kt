a66e4350e1241d57cb37fdcb15f6bcde95877734
Jeferson Coutinho <jefissuu@gmail.com>
2023-03-13 14:49:35 -0300

    add splash screen

--- app/src/main/java/com/jefisu/manualplus/MainActivity.kt
+++ app/src/main/java/com/jefisu/manualplus/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jefisu.manualplus.ui.theme.ManualPLUSTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             ManualPLUSTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
